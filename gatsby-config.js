053d5bbf5b214995ca25cfbe0232940b6a10db89
Spencer Graham <sl.graha@gmail.com>
2020-06-07 20:41:19 -0500

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Hi, I\'m Spencer',
+    author: 'Spencer Graham',
+    description: 'Hi, I\'m Spencer. Sometimes on the internet I go by spengrah.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Spencer-personal-site',
+        short_name: 'spengrah',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
