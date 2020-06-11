7e708cfcd46cb86809dcdb194f6eed59f7b84e6b
Spencer Graham <sl.graha@gmail.com>
2020-06-10 19:43:49 -0500

    customize footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Footer = (props) => (
-    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
-    </footer>
+const Footer = props => (
+  <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
+    <p className="copyright">
+      &copy; Spencer Graham, 2020. Built with the Gatsby Starter - Dimension
+      template by{' '} 
+      <a href="https://html5up.net" target="_blank" rel="noreferrer">
+        HTML5 UP
+      </a>
+    </p>
+  </footer>
 )
 
 Footer.propTypes = {
-    timeout: PropTypes.bool
+  timeout: PropTypes.bool,
 }
 
 export default Footer
