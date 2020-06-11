c6df4c81a7e8b7b7d292f3717146bee353a1b241
Spencer Graham <sl.graha@gmail.com>
2020-06-10 19:43:07 -0500

    customize favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,8 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/burrito.png', // This path is relative to the root of the site.
+        include_favicon: true
       },
     },
     'gatsby-plugin-sass',
