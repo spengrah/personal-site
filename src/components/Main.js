8506e3dc5705f6d18ef96c3c6604abf98ee761bc
Spencer Graham <sl.graha@gmail.com>
2020-06-10 20:29:04 -0500

    customize contact links

--- src/components/Main.js
+++ src/components/Main.js
@@ -118,48 +118,79 @@ class Main extends React.Component {
           <form method="post" action="#">
             <div className="field half first">
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
+              <input type="text" name="name" id="name" aria-labelledby="name" />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input
+                type="text"
+                name="email"
+                id="email"
+                aria-labelledby="email"
+              />
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
+              <textarea
+                name="message"
+                id="message"
+                rows="4"
+                aria-labelledby="message"
+              >
+                {' '}
+              </textarea>
             </div>
             <ul className="actions">
               <li>
-                <input type="submit" value="Send Message" className="special" />
+                <input
+                  type="submit"
+                  value="Send Message"
+                  className="special"
+                  aria-label="send message"
+                />
               </li>
               <li>
-                <input type="reset" value="Reset" />
+                <input type="reset" value="Reset" aria-label="reset" />
               </li>
             </ul>
           </form>
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href="https://twitter.com/spengrah"
+                target="_blank"
+                rel="noreferrer"
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
-                <span className="label">Instagram</span>
+              <a
+                href="https://medium.com/@spengrah"
+                className="icon fa-medium"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="label">Medium</span>
               </a>
             </li>
             <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
+                href="https://www.linkedin.com/in/spencergraham/"
+                className="icon fa-linkedin"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="label">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/slgraham"
                 className="icon fa-github"
+                target="_blank"
+                rel="noreferrer"
               >
                 <span className="label">GitHub</span>
               </a>
