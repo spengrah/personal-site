443193c0deff7fb75d834f9f7c9cb92d691bd857
Spencer Graham <sl.graha@gmail.com>
2020-06-21 11:39:17 -0500

    initial web3 commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,12 @@ import PropTypes from 'prop-types'
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
     <p className="copyright">
-      &copy; Spencer Graham, 2020. Built with the Gatsby Starter - Dimension
-      template by{' '} 
+      &copy; Spencer Graham, 2020. Built with Gatsby from the Dimension template
+      by{' '}
       <a href="https://html5up.net" target="_blank" rel="noreferrer">
         HTML5 UP
       </a>
+      .<br></br>Hosted on web3.
     </p>
   </footer>
 )
